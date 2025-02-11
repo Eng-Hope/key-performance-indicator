65a3bab3e0331d0e72a9bbafc7f73fd189ba3b44
Hope Richard Maleko <hoperichardmaleko@gmail.com>
2025-02-11 08:13:07 +0300

    change guard

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'api'),
+        'guard' => 'api',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
