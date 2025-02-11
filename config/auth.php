bf063e0d4910b92989f79e3fb3d4bb4dcf7e88f1
Hope Richard Maleko <hoperichardmaleko@gmail.com>
2025-02-11 08:19:12 +0300

    fix composer

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => env('AUTH_GUARD', 'api'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
